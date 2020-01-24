0a9000e5f7e70971a14c99e32b701246591954b5
robot144 <verlaanm@gmail.com>
2020-01-24 12:19:17 +0100

    Small change to Centos7 settings

--- settings/set_centos7_intel18.sh
+++ settings/set_centos7_intel18.sh
@@ -1,8 +1,11 @@
 #! /bin/sh
 
+echo "Settings for Centos7 with Intel 18 compiler"
 #module load intel/18.0.3
 . /opt/intel/18.0.3/bin/compilervars.sh -arch intel64 -platform linux 
 export LIBIFPORT=/opt/intel/18.0.3/compilers_and_libraries_2018.3.222/linux/compiler/lib/intel64_lin
+
+## no modules on devux
 #module load autoconf
 #module load automake
 #module load libtool
