36651ec79ee89f11cf296f195e05fb0677335398
Robot144 <robot144@protonmail.com>
2021-04-20 09:52:53 +0200

    Updated cleaning script.

--- clean.sh
+++ clean.sh
@@ -7,10 +7,10 @@ rm -rf *_linux64_ifort_temp
 rm -rf *_linux64_gnu_temp
 rm -rf hdf5-1.10.4
 rm -rf metis-5.1.0
-rm -rf mpich-3.3.1
+rm -rf mpich-3.3.*
 rm -rf netcdf-c-4.6.2
 rm -rf netcdf-fortran-4.4.4
-rm -rf petsc-3.9.4
+rm -rf petsc-3.*
 
 
 #rm -rf dflowfm-trunk
